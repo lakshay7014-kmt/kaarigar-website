a00c247822b7d1087d8ff3b041611aeb554a6122
lakshay7014-kmt <231536965+lakshay7014-kmt@users.noreply.github.com>
2026-07-19 14:11:08 +0530

    Fix icon paths in service worker

--- sw.js
+++ sw.js
@@ -9,8 +9,8 @@ const SHELL_FILES = [
   './customer-dashboard.html',
   './worker-dashboard.html',
   './manifest.json',
-  './icons/icon-192.png',
-  './icons/icon-512.png'
+  './icon-192.png',
+  './icon-512.png'
 ];
 
 self.addEventListener('install', (event) => {
